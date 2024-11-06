5fc2db1ba659dad1ffc44586540d8cd36a06d2cd
lior-yossef <lior.yossef@post.runi.ac.il>
2024-11-06 17:16:06 +0200

    Add Ascend, FVCalc and NumWords functionality
    Signed-off-by: lior-yossef <lior.yossef@post.runi.ac.il>

--- Ascend.java
+++ Ascend.java
@@ -2,6 +2,15 @@
 // and prints them in increasing order.
 public class Ascend {
 	public static void main(String[] args) {
-		// Replace this comment with your code
+		int lim = Integer.parseInt(args[0]);
+		int a = (int) (lim * Math.random());
+		int b = (int) (lim * Math.random());
+		int c = (int) (lim * Math.random());
+
+		int min = Math.min(a, Math.min(b, c));
+		int max = Math.max(a, Math.max(b, c));
+		int middle = a + b + c - min - max;
+
+		System.out.println(min + " " + middle + " " + max);
 	}
 }
--- FVCalc.java
+++ FVCalc.java
@@ -1,6 +1,11 @@
 // Computes the future value of a saving investment.
 public class FVCalc {
-	public static void main(String[] args){
-		// Replace this comment with your code
+	public static void main(String[] args) {
+		int moneySum = Integer.parseInt(args[0]);
+		double interestRate = Double.parseDouble(args[1]);
+		int years = Integer.parseInt(args[2]);
+		double moneyWithInterest = moneySum * Math.pow(1 + interestRate / 100, years);
+		System.out.println("After " + years + " years, $" + moneySum + " saved at " + interestRate + "% will yield $"
+				+ (int) moneyWithInterest);
 	}
 }
